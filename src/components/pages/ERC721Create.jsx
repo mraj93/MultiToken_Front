986460a1a465d8de91f8ec9e7e9eceae68e9fc67
Mukesh-iMac <mrajbhar009@gmail.com>
2023-08-01 18:29:57 +0530

    Image upload section is added

--- src/components/pages/ERC721Create.jsx
+++ src/components/pages/ERC721Create.jsx
@@ -1,97 +1,141 @@
 import React, { useState } from "react";
 import "../CSS/ERC721Create.css";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useForm } from 'react-hook-form';
+import axios from "axios";
+import ReactLoading from "react-loading";
+import {Buffer} from "buffer";
+import {Upload} from "antd";
+window.Buffer = Buffer;
+
+const { PROJECT_ID, INFURA_KEY, INFURA_ENDPOINT } = process.env;
+const authorization = "Basic " + Buffer.from(PROJECT_ID + ":" + INFURA_KEY).toString("base64");
 
 const ERC721Create = () => {
-  const [nftName, setNftName] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [imageHash, setImageHash] = useState("");
+    const { register, handleSubmit} = useForm();
+    const ipfs = ipfsHttpClient({
+        url: INFURA_ENDPOINT,
 
-  const handleCategoryChange = (e) => {
-    setNftName(e.target.value);
-  };
-
-  const handlePriceChange = (e) => {
-    setPrice(e.target.value);
-  };
-
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
-  };
-  
-  const MintNFT = () => {
-    console.log("Mint Success with parameters", {
-      setNftName,
-      description,
-      price,
+        headers: {
+            authorization,
+        },
     });
-  };
-  const handleImageChange = () => {
-    console.log("Image Uploaded");
-  };
 
-  const handleFileChange = () => {
-    console.log("Image Uploaded");
-  };
+    const submitData = async (data) => {
+        console.log("data", data);
+        setIsLoading(true);
+        try {
+            const response = await axios.post('http://localhost:9090/mintERC721', data, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        setIsLoading(false);
+    };
 
-  return (
-    <>
-      <div className="title">
-        <h1>Mint NFT</h1>
-      </div>
-      <div className="mint-nft-page">
-        <form>
-          <div className="form-container">
-            <div className="form-group w-100">
-              <label className="image-upload1">Upload Image</label>
-              <input
-                className="image-upload"
-                type="file"
-                id="image"
-                onChange={handleImageChange}
-              />
+    const beforeUpload = (file) => {
+        const isPNG = file.type === "image/jpg" || file.type === "image/png" || file.type === "image/jpeg" || file.type === "image/gif";
+        const isSizeValid = file.size / 1024 / 1024 <= 10;
+        if ( (!isPNG) || (!isSizeValid) ) {
+            alert("File format or size is not acceptable");
+        }
+        return (isPNG && isSizeValid);
+    };
+
+    const handleImageUpload = async (e) => {
+        try {
+            const file = e.target.files[0];
+            const { cid } = await ipfs.add(file);
+            setImageHash(cid.toString());
+            console.log("image hash is here", cid.toString());
+        }
+        catch (e) {
+            console.error(e)
+        }
+    }
+
+    return (
+        <>
+            <div className={`overlay ${isLoading ? 'active' : ''}`}>
+                {isLoading && (
+                    <div className="loader-container">
+                        <p><strong>NFT is minting...</strong></p>
+                        <ReactLoading type="spin" color="cyan" height={60} width={70}/>
+                    </div>
+                )}
             </div>
 
-            <div className="form-group w-100">
-              <label className="left-align-label">Name</label>
-              <input
-                className="input-field"
-                type="text"
-                id="category"
-                value={nftName}
-                onChange={handleCategoryChange}
-              />
+            <div className="title">
+                <h1>Mint NFT</h1>
             </div>
-            <div className="form-group">
-              <label>Price</label>
-              <input
-                className="input-field"
-                type="text"
-                id="price"
-                value={price}
-                onChange={handlePriceChange}
-              />
+            <div className="mint-nft-page">
+                <form onSubmit={handleSubmit(submitData)}>
+                    <div className="form-container">
+
+                        <div className="form-group w-100">
+                            <label className="image-upload1">Upload Image</label>
+                            <Upload
+                                className="image-upload"
+                                type="file"
+                                onChange={handleImageUpload}
+                                beforeUpload={beforeUpload}
+                            />
+                        </div>
+
+                        {/*<div className="form-group w-100">*/}
+                        {/*  <label className="left-align-label">Name</label>*/}
+                        {/*  <input*/}
+                        {/*      className="input-field"*/}
+                        {/*      type="text"*/}
+                        {/*      {...register('nftName')}*/}
+                        {/*  />*/}
+                        {/*</div>*/}
+
+                        <div className="form-group w-100">
+                            <label className="left-align-label">Name</label>
+                            <input
+                                className="input-field"
+                                type="text"
+                                {...register('nftName')}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Price</label>
+                            <input
+                                className="input-field"
+                                type="text"
+                                {...register('price')}
+                            />
+                        </div>
+                        <div className="form-group w-100">
+                            <label>Description</label>
+                            <textarea
+                                className="textarea-field"
+                                {...register('description')}
+                            />
+                        </div>
+                        <div className="form-group w-100">
+                            <label className="left-align-label">TokenURI</label>
+                            <input
+                                className="input-field"
+                                type="text"
+                                {...register('tokenURI')}
+                            />
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary w-25 mint-button">
+                        Mint NFT
+                    </button>
+                </form>
             </div>
-            <div className="form-group w-100">
-              <label>Description</label>
-              <textarea
-                className="textarea-field"
-                id="description"
-                value={description}
-                onChange={handleDescriptionChange}
-              />
-            </div>
-          </div>
-          <button
-            type="button "
-            className="btn btn-primary w-25 mint-button"
-            onClick={MintNFT}
-          >
-            Mint NFT
-          </button>
-        </form>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default ERC721Create;
