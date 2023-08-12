b079df2e9de98d3ac8995fc78c8950ded76f2d72
Mukesh <mrajbhar009@gmail.com>
2023-08-12 12:03:06 +0530

    config added

--- craco.config.js
+++ craco.config.js
@@ -1,29 +1,3 @@
-// const webpack = require('webpack');
-// const dotenv = require('dotenv').config();
-//
-// const environmentVariables = {};
-// for (const key in dotenv.parsed) {
-//     environmentVariables[`process.env.${key}`] = JSON.stringify(dotenv.parsed[key]);
-// }
-//
-// module.exports = {
-//     webpack: {
-//         configure: (webpackConfig) => {
-//             if (!webpackConfig.resolve.fallback) {
-//                 webpackConfig.resolve.fallback = {};
-//             }
-//             webpackConfig.resolve.fallback.crypto = require.resolve('crypto-browserify');
-//
-//             // Add environment variables to the DefinePlugin
-//             webpackConfig.plugins.push(
-//                 new webpack.DefinePlugin(environmentVariables)
-//             );
-//
-//             return webpackConfig;
-//         },
-//     },
-// };
-
 const webpack = require('webpack');
 const dotenv = require('dotenv').config();
 
@@ -42,13 +16,8 @@ module.exports = {
 
             // Add environment variables to the DefinePlugin
             webpackConfig.plugins.push(
-                new webpack.DefinePlugin({
-                    ...environmentVariables, // Keep existing environment variables
-                    'BUILD_AT': JSON.stringify(Date.now().toString(32)),
-                    'DEBUG': process.env.NODE_ENV !== 'production',
-                })
+                new webpack.DefinePlugin(environmentVariables)
             );
-
             return webpackConfig;
         },
     },
