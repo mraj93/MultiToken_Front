700de21fbf4c80365a522e91d5051a0d302d23f7
Mukesh <mrajbhar009@gmail.com>
2023-08-12 10:46:36 +0530

    Homepage route uncommented

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ const App = () => {
         <Router>
             <Header />
             <Routes>
-                {/*<Route path="/" element={<HomePage />} />*/}
+
+                <Route path="/" element={<HomePage />} />
 
                 <Route path="/create/NFT" element={<ERC721Create />} />
                 <Route path="/create/ERC1155" element={<ERC1155Create />} />
